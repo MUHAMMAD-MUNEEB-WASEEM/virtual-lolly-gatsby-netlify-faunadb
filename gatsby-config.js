d326496c6f0ae107ec7dc642b9a1018fcd17e258
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2021-01-06 18:37:48 +0500

    programmatically create pages using build hooks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,24 @@
-  
 /**
  * Configure your Gatsby site with this file.
  *
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
-
 module.exports = {
-    /* Your site config here */
-    plugins: [],
-  }
+  /* Your site config here */
+  plugins: [
+    "gatsby-plugin-typescript",
+    `gatsby-plugin-material-ui`,
+    {
+      resolve: "gatsby-source-graphql",
+      options: {
+        // This type will contain remote schema Query type
+        typeName: "lolly",
+        // This is field under which it's accessible
+        fieldName: "getAllLollies",
+        // Url to query from
+        url:
+          "https://virtual-lolly-by-muneeb.netlify.app/.netlify/functions/lollies",
+      },
+    },
+  ],
+}
